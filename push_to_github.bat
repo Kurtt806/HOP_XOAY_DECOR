642ccf334184b1314a0222ae613c34406c1a8a3d
KHOA <93168474+Kurtt806@users.noreply.github.com>
2025-04-16 20:40:45 +0700

    auto up

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,25 +1,23 @@
 @echo off
 cd /d %~dp0
-echo 🔄 Đang chạy Git Auto Push Script...
+echo Git Auto Push Script...
 
 :: Đặt tên commit
-set COMMIT_MSG=up step
+set COMMIT_MSG=auto up
 
 :: Bước 1: git add .
 git add .
-echo ✅ Đã add tất cả file
+echo add all file
 
 :: Bước 2: commit
 git commit -m "%COMMIT_MSG%"
-echo ✅ Commit với nội dung: %COMMIT_MSG%
+echo Commit content: %COMMIT_MSG%
 
 :: Bước 3: kiểm tra xem remote đã tồn tại chưa
 git remote show origin >nul 2>&1
 if errorlevel 1 (
-    echo 🔗 Thêm remote origin...
+    echo link remote origin...
     git remote add origin https://github.com/Kurtt806/HOP_XOAY_DECOR.git
-) else (
-    echo 🔗 Remote origin đã tồn tại, bỏ qua...
 )
 
 :: Bước 4: đảm bảo nhánh chính là main
@@ -28,5 +26,5 @@ git branch -M main
 :: Bước 5: push lên GitHub
 git push -u origin main
 
-echo 🎉 Hoàn tất Git Push!
+echo DONEEEEEEEEEEEE
 pause
